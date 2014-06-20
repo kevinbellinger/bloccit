f3a66cf25c390364097a2def0d25f10e880772dc
Kevin Bellinger <kevinb@outlook.com>
2014-06-20 15:24:57 +0000

    Associated users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,46 @@
 # Adding code for faker data - 20.06.2014
-
 require 'faker'
 
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save
+end
+users = User.all
+
+
 # Create Posts
-50.times do 
+50.times do
   Post.create(
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph
-    )
+    user:   users.sample,
+    title:  Faker::Lorem.sentence,
+    body:   Faker::Lorem.paragraph
+  )
 end
 posts = Post.all
 
 # Create Comments
 100.times do
   Comment.create(
+    # user: users.sample,   # we have not yet associated Users with Comments
     post: posts.sample,
     body: Faker::Lorem.paragraph
-    )
-end
+  )
+User.first.update_attributes(
+  email: 'kevin@glider.io',
+  password: 'Password',
+)
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+
+
+
+end
