ac3cd68d69d747dee42807478f5dfa054c0407c3
Kevin Bellinger <kevinb@outlook.com>
2014-06-29 08:49:15 +0000

    Created Popular Posts and Users, and optimized.

--- Gemfile
+++ Gemfile
@@ -64,6 +64,9 @@ gem 'fog'
 #Adding Pagination
 gem 'will_paginate', '~> 3.0.5'
 
+#Adding Pry
+#gem 'pry'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 
   get 'about' => "welcome#about"
 
+  resources :posts, only: [:index]
   
   resources :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments, only: [:create, :destroy]
       get '/up-vote' => 'votes#up_vote', as: :up_vote
       get '/down-vote' => 'votes#down_vote', as: :down_vote
@@ -15,7 +16,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
-  resources :users, only: [ :show, :update]
+  resources :users, only: [ :show, :index, :update]
 
 
 
